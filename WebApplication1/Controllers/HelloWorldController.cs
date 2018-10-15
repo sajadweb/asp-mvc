440235f10f3e5ac83d261905a385f6f26f16884b
Sajjad Mohammadi <radhoosh.user2@gmail.com>
2018-10-15 08:57:08 +0330

    start

---
+++ WebApplication1/Controllers/HelloWorldController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
+    public class HelloWorldController : Controller
+    {
+        public int Id { get; set; }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("welcome");
+        }
+
+        // 
+        // GET: Does not use the name of the action whene that route without [actions]
+        // GET: /api/HelloWorld/1
+        [HttpGet("{id}")]
+        public IActionResult Welcome(int id)
+        {
+            return Ok("Welcome " + id);
+        }
+    }
+}
